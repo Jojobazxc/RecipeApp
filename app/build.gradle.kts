aa749b5a22f359b5e0512ef9fef2c530f64965d0
Rodion <piperov2004@mail.ru>
2024-02-14 20:33:43 +0300

    RA-03 Navigation buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.recipeapp"
     compileSdk = 34
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.recipeapp"
         minSdk = 28
