35e381e887d115c4cffd41680517195e9983f533
Rodion <piperov2004@mail.ru>
2024-02-14 23:34:06 +0300

    RA-03 Navigation buttons2

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.recipeapp.databinding.ActivityMainBinding
@@ -12,5 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        window.statusBarColor = Color.parseColor("#F4FAFF")
+        window.navigationBarColor = Color.parseColor("#F4FAFF")
     }
 }
