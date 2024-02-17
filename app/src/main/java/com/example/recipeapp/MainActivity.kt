58e5cf0cbd7d773d1ef134182041e2e5dac3f932
Rodion <piperov2004@mail.ru>
2024-02-17 14:03:00 +0300

    RA-04 commit error

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
 import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val fragment = CategoriesListFragment()
-        val fragmentTransaction = supportFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.mainContainer, fragment).commit()
+        supportFragmentManager.commit {
+
+        }
+
     }
 }
