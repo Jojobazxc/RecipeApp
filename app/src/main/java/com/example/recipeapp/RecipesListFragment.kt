edc3c8016d5157c2c8f68950036e750bcc7f0d79
Rodion <piperov2004@mail.ru>
2024-02-27 14:19:22 +0300

    RA-10 some changes

--- app/src/main/java/com/example/recipeapp/RecipesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/RecipesListFragment.kt
@@ -26,10 +26,10 @@ class RecipesListFragment : Fragment(R.layout.fragment_list_recipes) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        requireArguments().let { args ->
-            categoryId = args.getInt(ARG_CATEGORY_ID)
-            categoryName = args.getString(ARG_CATEGORY_NAME)
-            categoryImageUrl = args.getString(ARG_CATEGORY_IMAGE_URL)
+        arguments.let { args ->
+            categoryId = args?.getInt(ARG_CATEGORY_ID)
+            categoryName = args?.getString(ARG_CATEGORY_NAME)
+            categoryImageUrl = args?.getString(ARG_CATEGORY_IMAGE_URL)
         }
 
     }
