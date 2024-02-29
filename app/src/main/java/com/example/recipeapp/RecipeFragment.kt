d8a1ec2f1dba686d2380f3f06935ab3c64bd4299
Rodion <piperov2004@mail.ru>
2024-02-29 15:43:53 +0300

    RA-12 implement the parcelable interface

--- app/src/main/java/com/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/RecipeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,10 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.databinding.FragmentRecipeBinding
 
-class RecipeFragment: Fragment(R.layout.fragment_favorites) {
+class RecipeFragment: Fragment(R.layout.fragment_recipe) {
     private val binding: FragmentRecipeBinding by lazy {
         FragmentRecipeBinding.inflate(layoutInflater)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,6 +23,13 @@ class RecipeFragment: Fragment(R.layout.fragment_favorites) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recipe: Recipe? = if (Build.VERSION.SDK_INT >= 33) {
+            arguments?.getParcelable(ARG_RECIPE, Recipe::class.java)
+        } else {
+            arguments?.getParcelable(ARG_RECIPE)
+        }
+        binding.tvRecipeTitle.text = recipe?.title
+
     }
 }
 
