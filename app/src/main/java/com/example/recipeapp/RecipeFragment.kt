ef61fac05ad50c00d3e6d5fc015f85feaf7caec7
Rodion <piperov2004@mail.ru>
2024-03-01 18:13:24 +0300

    RA-13 layout of the recipe screen has been done

--- app/src/main/java/com/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/com/example/recipeapp/RecipeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.recipeapp
 
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.recipeapp.databinding.FragmentRecipeBinding
+import java.io.IOException
+import java.io.InputStream
 
 class RecipeFragment: Fragment(R.layout.fragment_recipe) {
     private val binding: FragmentRecipeBinding by lazy {
@@ -28,7 +32,16 @@ class RecipeFragment: Fragment(R.layout.fragment_recipe) {
         } else {
             arguments?.getParcelable(ARG_RECIPE)
         }
-        binding.tvRecipeTitle.text = recipe?.title
+        binding.tvHeaderTitle.text = recipe?.title
+        try {
+            val categoryImageUrl = recipe?.imageUrl
+            val inputStream: InputStream? =
+                categoryImageUrl?.let { binding.ivHeaderImage.context?.assets?.open(it) }
+            val drawable: Drawable? = Drawable.createFromStream(inputStream, null)
+            binding.ivHeaderImage.setImageDrawable(drawable)
+        } catch (e: IOException) {
+            Log.e("RecipeApp", "${e.printStackTrace()}", e)
+        }
 
     }
 }
